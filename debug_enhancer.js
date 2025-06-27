2b6fa998a0665def06180f3f12a0f3556305edde
Sonar-glitch <furqanz.email@gmail.com>
2025-06-27 19:28:04 -0400

    Add enhancer diagnostic script

---
+++ debug_enhancer.js
@@ -0,0 +1,6 @@
+const { RecommendationEnhancer } = require('./lib/recommendationEnhancer');
+const enhancer = new RecommendationEnhancer();
+console.log('Enhancer enabled:', enhancer.enabled);
+console.log('Enhancer methods:', Object.getOwnPropertyNames(Object.getPrototypeOf(enhancer)));
+console.log('needsEnhancement method exists:', typeof enhancer.needsEnhancement);
+console.log('enhanceEvent method exists:', typeof enhancer.enhanceEvent);
